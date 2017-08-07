d85488199ff8428e82c28a4a867d3a8ffe3cf32e
dhivya97 <30342650+dhivya97@users.noreply.github.com>
2017-08-07 12:24:52 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int n, i;
+     printf("Enter an integer: ");
+    scanf("%d",&n);
+
+  
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+
+    return0;
+        
+    }
