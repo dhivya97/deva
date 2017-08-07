2a8509185b37b0440f6c7114d092a95d2fb549bf
dhivya97 <30342650+dhivya97@users.noreply.github.com>
2017-08-07 12:15:25 +0530

    Rename 16.c to 17.c

---
+++ 17.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf("enter the three number: ");
+    scanf("%d", &number);
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+}
